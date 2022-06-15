03d63783ba98cb7b46392900d4207c6162980ce7
MKhimukhin <mikhail.khimukhin@dxc.com>
2022-06-15 16:54:28 +0300

    test images fixed

--- test_images.py
+++ test_images.py
@@ -13,11 +13,13 @@ from utils.comparators import compare_images
 )
 def test_jpg(baseline_path, subject_path, result_path):
 
-    baseline_images = sorted(path for path in os.listdir(baseline_path) if path.endswith(".jpg"))
-    subject_images = sorted(path for path in os.listdir(subject_path) if path.endswith(".jpg"))
-    for baseline_image_path, subject_image_path in zip(
-        baseline_images, subject_images
-    ):
+    baseline_images = sorted(
+        path for path in os.listdir(baseline_path) if path.endswith(".jpg")
+    )
+    subject_images = sorted(
+        path for path in os.listdir(subject_path) if path.endswith(".jpg")
+    )
+    for baseline_image_path, subject_image_path in zip(baseline_images, subject_images):
         compare_images(
             os.path.join(baseline_path, baseline_image_path),
             os.path.join(subject_path, subject_image_path),
@@ -33,11 +35,13 @@ def test_jpg(baseline_path, subject_path, result_path):
 )
 def test_png(baseline_path, subject_path, result_path):
 
-    baseline_images = sorted(path for path in os.listdir(baseline_path) if path.endswith(".jpg"))
-    subject_images = sorted(path for path in os.listdir(subject_path) if path.endswith(".jpg"))
-    for baseline_image_path, subject_image_path in zip(
-        baseline_images, subject_images
-    ):
+    baseline_images = sorted(
+        path for path in os.listdir(baseline_path) if path.endswith(".png")
+    )
+    subject_images = sorted(
+        path for path in os.listdir(subject_path) if path.endswith(".png")
+    )
+    for baseline_image_path, subject_image_path in zip(baseline_images, subject_images):
         compare_images(
             os.path.join(baseline_path, baseline_image_path),
             os.path.join(subject_path, subject_image_path),
